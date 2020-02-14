799abadac278f3f5f9c3cc6fc689022ed6413681
Stuart McKay <stuart51994@gmail.com>
2020-02-13 17:30:46 -0800

    create read_a_task controller

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -19,3 +19,11 @@ exports.create_a_task = function(req, res) {
     res.json(task);
   });
 };
+
+exports.read_a_task = function(req, res) {
+  Task.findById(req.params.taskId, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
