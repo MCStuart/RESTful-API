28a0bb0a47682782125acbb53ddff153715e3cb7
Stuart McKay <stuart51994@gmail.com>
2020-02-13 17:29:55 -0800

    add create_a_task controller

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -10,3 +10,12 @@ exports.list_all_tasks = function(req, res) {
     res.json(task);
   });
 };
+
+exports.create_a_task = function(req, res) {
+  var new_task = new Task(req.body);
+  new_task.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
